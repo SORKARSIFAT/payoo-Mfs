0334d7534154a5ef9894a1548cf9edb2469a7196
SORKARSIFAT <mominulsorkar971@gmail.com>
2025-02-19 00:12:11 +0600

    hhhh

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 // console.log("hello web this come js you warn");
+const body = document.getElementById('body')
 document.getElementById("btn-login").addEventListener("click" , function(e){
 //  prevent default behavior (auto load stop from html teg )
 e.preventDefault();  
@@ -6,6 +7,15 @@ e.preventDefault();
 
    const phoneNumber = document.getElementById("phone-number").value;
    const pinNumber = document.getElementById("pin-number").value
-    console.log(phoneNumber , pinNumber);
+  if(phoneNumber === '01708063559' && pinNumber === '1234'){
+    window.location.href = '/homePage.html'
+    
+  }
+
+  else{
+ 
+   alert('wrong your pin or number');
+   
+  }
     
 })
